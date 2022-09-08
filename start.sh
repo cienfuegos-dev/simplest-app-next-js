62594673ae21b550dd78dd17425ad96f77895644
cienfuegos-dev <cienfuegos.dev@gmail.com>
2022-09-08 12:55:18 -0500

    Add docs

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+# - Generates the env-inject.js using the system env vars
+# - Then sets read-only permissions to env-inject.js
+# - Starts the server
+
 : \
   && (chmod -f 600 /app/public/env-inject/env-inject.js || :) \
   && /app/env-inject.js.sh > /app/public/env-inject/env-inject.js \
