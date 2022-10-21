74817fe41191d25f066cde61bde4441d40c10d34
cienfuegos-dev <cienfuegos.dev@gmail.com>
2022-10-20 22:56:29 -0500

    Fix hostname. Set it before generating env-inject.

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,9 +3,9 @@
 # - Starts the server
 
 : \
+  && APP_HOSTNAME=$(hostname -f) \
   && (chmod -f 600 /app/public/env-inject/env-inject.js || :) \
   && /app/deploy/env-inject.js.sh > /app/public/env-inject/env-inject.js \
   && echo File env-inject.js generated. \
   && chmod 400 /app/public/env-inject/env-inject.js \
-  && APP_HOSTNAME=$(hostname -f) \
   && node server.js
